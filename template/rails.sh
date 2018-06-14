df29c3d11fcb28673cdf04ec7f181aa47276ca0b
tagusa-k <tagusa-k@t-axis.co.jp>
2018-06-14 17:01:32 +0900

    Rails の依存パッケージインストールに -y オプションが抜けていたのを修正

--- template/rails.sh
+++ template/rails.sh
@@ -7,7 +7,7 @@
 firewall-cmd --add-port=3000/tcp --zone=public --permanent
 firewall-cmd --reload
 
-yum install nodejs
+yum install -y nodejs
 
 mysql_password=$(grep "password" /etc/motd | awk -F: '{print $2}' | tr -d ' ')
 mysql -uroot -p$mysql_password -e "UPDATE mysql.user SET password='' WHERE User='root'"
