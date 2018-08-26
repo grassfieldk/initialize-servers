01c061f41f8498caf4875175335ed7d27e2e8139
grassfield <gtemacomp@gmail.com>
2018-08-26 12:39:30 +0900

    Rails アプリケーションサーバの DB 初期パスワード変更処理を修正

--- template/rails.sh
+++ template/rails.sh
@@ -9,6 +9,9 @@ firewall-cmd --reload
 
 yum install -y nodejs
 
-mysql_password=$(grep "password" /etc/motd | awk -F: '{print $2}' | tr -d ' ')
-mysql -uroot -p$mysql_password -e "UPDATE mysql.user SET password='' WHERE User='root'"
-mysql -uroot -p$mysql_password -e "FLUSH PRIVILEGES"
+# Change MariaDB root user password
+echo -n "MySQL root password: "
+read DB_NEW_PASSWORD
+DB_DEFAULT_PASSWORD=$(grep "root password" /etc/motd | tr -d " " | cut -d ":" -f 2)
+mysql -uroot -p${DB_DEFAULT_PASSWORD} -e "UPDATE mysql.user SET Password=PASSWORD('${DB_NEW_PASSWORD}') WHERE User='root'"
+mysql -uroot -p${DB_DEFAULT_PASSWORD} -e "FLUSH PRIVILEGES"
